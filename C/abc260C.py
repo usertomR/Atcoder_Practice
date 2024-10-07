46928acee53bd9eb8ed70f47a46c9de483773e54
usertomR <ryusuke.tomita.engineer@gmail.com>
2024-10-07 22:39:28 +0900

    コード量は回答スピードに直結。再回答で20分減らせた。abcでは方針を複数考え、コード量最小の方針で回答したい。

--- C/abc260C.py
+++ C/abc260C.py
@@ -1,36 +1,16 @@
 N, X, Y = map(int, input().split())
 
-red_levelOne_only = False
-blue_levelOne_Only = False
-red_count_level_Arr = [0] * (N + 1)
-blue_count_level_Arr = [0] * (N + 1)
-red_count_level_Arr[N] = 1
+red_Arr = [0] * (N + 1)
+blue_Arr = [0] * (N + 1)
+red_Arr[N] = 1
 
-while not(red_levelOne_only == True and blue_levelOne_Only == True):
-  for red_level in reversed(range(2, N + 1)):
-    if red_count_level_Arr[red_level] >= 1:
-      red_count_level_Arr[red_level - 1] += 1 * red_count_level_Arr[red_level]
-      blue_count_level_Arr[red_level] += X * red_count_level_Arr[red_level]
-      
-      red_count_level_Arr[red_level] = 0
-  
-  for blue_level in reversed(range(2, N + 1)):
-    if blue_count_level_Arr[blue_level] >= 1:
-      red_count_level_Arr[blue_level - 1] += 1 * blue_count_level_Arr[blue_level]
-      blue_count_level_Arr[blue_level - 1] += Y * blue_count_level_Arr[blue_level]
+for level in reversed(range(2, N + 1)):
+  red_Arr[level - 1] += red_Arr[level]
+  blue_Arr[level] += X * red_Arr[level]
+  red_Arr[level] = 0
 
-      blue_count_level_Arr[blue_level] = 0
-  
-  red_levelOne_only = True
-  blue_levelOne_Only = True
-  for red_level in reversed(range(2, N + 1)):
-    if red_count_level_Arr[red_level] >= 1:
-      red_levelOne_only = False
-      break
-  
-  for blue_level in reversed(range(2, N + 1)):
-    if blue_count_level_Arr[blue_level] >= 1:
-      blue_levelOne_Only = False
-      break
+  red_Arr[level - 1] += blue_Arr[level]
+  blue_Arr[level - 1] += Y * blue_Arr[level]
+  blue_Arr[level] = 0
 
-print(blue_count_level_Arr[1])
+print(blue_Arr[1])
